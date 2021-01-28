e152087d44621bb0ee17154d205bf56b422f2d69
sarahjaynekessler <44033444+sarahjaynekessler@users.noreply.github.com>
2021-01-28 17:32:08 -0500

    added a MAD to pandas functions

--- pandasFunctions.py
+++ pandasFunctions.py
@@ -1,13 +1,29 @@
 import pandas as pd
+import numpy as np
 
-def rollingmedainXY(df,X,Y):
-	data_cut = pd.cut(df.X,bins)           #we cut the data following the bins
-	grp = df.groupby(by = data_cut)        #we group the data by the cut
-	ret = grp.aggregate(np.median)         #we produce an aggregate representation (median) of each bin
-	return(ret)
+def rollingmedainXY(df,X,bins):
+    df2 = df.select_dtypes(include=np.number)
+    data_cut = pd.cut(df2[X],bins)
+    grp = df2.groupby(by = data_cut)
+    ret = grp.aggregate(np.nanmedian)
+    return(ret)
 
-def rollingstdXY(df,X,Y):
-	data_cut = pd.cut(df.X,bins)           #we cut the data following the bins
-	grp = df.groupby(by = data_cut)        #we group the data by the cut
-	ret = grp.aggregate(np.std)         #we produce an aggregate representation (std) of each bin
-	return(ret)
+def rollingstdXY(df,X,bins):
+    df2 = df.select_dtypes(include=np.number)
+    data_cut = pd.cut(df2[X],bins)           #we cut the data following the bins
+    grp = df2.groupby(by = data_cut)        #we group the data by the cut
+    ret = grp.aggregate(np.nanstd)         #we produce an aggregate representation (std) of each bin
+    return(ret)
+
+def rollingMADXY(df,X,bins):
+    df2 = df.select_dtypes(include=np.number)
+    data_cut = pd.cut(df2[X],bins)           #we cut the data following the bins
+    grp = df2.groupby(by = data_cut)        #we group the data by the cut
+    ret = grp.aggregate(myMAD)         #we produce an aggregate MAD per bin
+    return(ret)
+
+def myMAD(x): #my function for MAD that ignored nans
+    med = np.nanmedian(x) #calculate median of bin
+    x   = abs(x-med) #fins absolute difference
+    MAD = np.nanmedian(x) #take median of the differences per bin
+    return(MAD)
